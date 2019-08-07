7fba0d160806b2a1552df8109521357b3ae0291a
Riguz Lee <solee.linux@gmail.com>
2019-08-07 09:13:11 +0800

    updated list/dropdown ui

--- ben_app/lib/pages/item_list.dart
+++ ben_app/lib/pages/item_list.dart
@@ -18,6 +18,7 @@ const List<DropdownMenuItem> menuItems = const <DropdownMenuItem>[
   const DropdownMenuItem(title: '关于', icon: Icons.directions_car),
   const DropdownMenuItem(title: '分享', icon: Icons.directions_bus),
   const DropdownMenuItem(title: '反馈', icon: Icons.directions_railway),
+  const DropdownMenuItem(title: '偏好设置', icon: Icons.directions_walk),
   const DropdownMenuItem(title: '检查更新', icon: Icons.directions_walk),
 ];
 
@@ -29,30 +30,40 @@ class _ItemListPageState extends State<ItemListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Stack(
-          children: <Widget>[
-            Center(
-              child: SearchBar(),
-
+        appBar: AppBar(
+          title: Stack(
+            children: <Widget>[
+              Center(
+                child: SearchBar(),
+              )
+            ],
+          ),
+          actions: <Widget>[
+            new IconButton(icon: Icon(AliIcon.help), onPressed: _onPressed),
+            new PopupMenuButton<DropdownMenuItem>(
+              onSelected: _onDropdownSelected,
+              itemBuilder: (BuildContext context) {
+                return menuItems.map((DropdownMenuItem choice) {
+                  return new PopupMenuItem<DropdownMenuItem>(
+                    value: choice,
+                    child: ListTile(
+                      leading: Icon(choice.icon),
+                      title: Text(choice.title),
+                    ),
+                  );
+                }).toList();
+              },
             )
           ],
         ),
-        actions: <Widget>[
-          new IconButton(icon: Icon(AliIcon.help), onPressed: _onPressed),
-          new PopupMenuButton<DropdownMenuItem>(
-            onSelected: _onDropdownSelected,
-            itemBuilder: (BuildContext context) {
-              return menuItems.map((DropdownMenuItem choice) {
-                return new PopupMenuItem<DropdownMenuItem>(
-                  value: choice,
-                  child: new Text(choice.title),
-                );
-              }).toList();
-            },
-          )
-        ],
-      ),
-    );
+        body: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                  child: ListTile(
+                      leading: Icon(AliIcon.group),
+                      title: Text('建设银行信用卡'),
+                      trailing: Icon(AliIcon.settings)));
+            }));
   }
 }
