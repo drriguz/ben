594e18592effe861d5746cd15f5fe73845f6a539
Riguz Lee <solee.linux@gmail.com>
2019-09-12 23:10:18 +0800

    added protected value

--- ben_app/test/model/protected_value_test.dart
+++ ben_app/test/model/protected_value_test.dart
@@ -7,4 +7,14 @@ void main() {
     expect(p.getText(), "hello world");
     expect(p.binaryValue.lengthInBytes, 11);
   });
+
+  test('equal and hash code', () {
+    final ProtectedValue p = ProtectedValue.of("hello world");
+    final ProtectedValue p1 = ProtectedValue.of("hello world1");
+    final ProtectedValue p2 = ProtectedValue.of("hello world");
+    expect(p == p1, false);
+    expect(p == p2, true);
+    expect(p.hashCode == p1.hashCode, false);
+    expect(p.hashCode == p2.hashCode, true);
+  });
 }
