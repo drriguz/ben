2390ba24440f8729fdfe2324fd04a8242a02c856
Riguz Lee <solee.linux@gmail.com>
2019-09-18 09:49:36 +0800

    updated initialize page layout.

--- ben_app/lib/ui/page/initialize.dart
+++ ben_app/lib/ui/page/initialize.dart
@@ -3,6 +3,49 @@ import 'package:flutter/material.dart';
 class InitializePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: const Text('Welcome to okapia!')));
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/title.png"),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.only(top: 80.0, left: 20, right: 20),
+                child: Column(
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage("assets/title.png"),
+                    ),
+                    TabBar(
+                      unselectedLabelColor: Colors.grey,
+                      labelColor: Colors.red,
+                      tabs: <Widget>[
+                        Tab(
+                          text: "了解难知",
+                        ),
+                        Tab(
+                          text: "用户协议",
+                        ),
+                        Tab(
+                          text: "系统设置",
+                        ),
+                        Tab(
+                          text: "完成",
+                        ),
+                      ],
+                    ),
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
   }
 }
