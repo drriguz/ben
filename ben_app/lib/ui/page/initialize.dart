ee7cecf6645936e63b90210c8d5a82bbc34d37fe
Riguz Lee <solee.linux@gmail.com>
2019-09-24 08:59:41 +0800

    refactor of init page

--- ben_app/lib/ui/page/initialize.dart
+++ ben_app/lib/ui/page/initialize.dart
@@ -1,50 +1,102 @@
 import 'package:flutter/material.dart';
 
-class InitializePage extends StatelessWidget {
+import 'component/about.dart';
+
+class InitializePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/title.png"),
-                  fit: BoxFit.contain,
-                ),
+  _InitializePageState createState() => _InitializePageState();
+}
+
+class _InitializePageState extends State<InitializePage>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(vsync: this, length: 4);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  Widget _createBackground() {
+    return Opacity(
+      opacity: 0.8,
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/green.jpeg"),
+            fit: BoxFit.cover,
+            alignment: Alignment.topCenter,
+          ),
+        ),
+      ),
+    );
+  }
+
+  static const tabNames = ["了解难知", "系统设置", "用户协议", "完成"];
+
+  Widget _createTabBar() {
+    return TabBar(
+      controller: _tabController,
+      isScrollable: true,
+      unselectedLabelColor: Colors.grey,
+      labelColor: Colors.red,
+      tabs: tabNames.map((tabName) => Tab(text: tabName)).toList(),
+    );
+  }
+
+  Widget _createTabs() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 100.0, left: 20, right: 20),
+      child: Column(
+        children: <Widget>[
+          Image(
+            image: AssetImage("assets/title.png"),
+          ),
+          _createTabBar(),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: TabBarView(
+                controller: _tabController,
+                children: <Widget>[
+                  AboutPage(),
+                  Text('2'),
+                  Text('3'),
+                  Text('4'),
+                ],
               ),
             ),
-            Padding(
-                padding: const EdgeInsets.only(top: 80.0, left: 20, right: 20),
-                child: Column(
-                  children: <Widget>[
-                    Image(
-                      image: AssetImage("assets/title.png"),
-                    ),
-                    TabBar(
-                      unselectedLabelColor: Colors.grey,
-                      labelColor: Colors.red,
-                      tabs: <Widget>[
-                        Tab(
-                          text: "了解难知",
-                        ),
-                        Tab(
-                          text: "用户协议",
-                        ),
-                        Tab(
-                          text: "系统设置",
-                        ),
-                        Tab(
-                          text: "完成",
-                        ),
-                      ],
-                    ),
-                  ],
-                )),
-          ],
-        ),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FlatButton(
+                child: Text("上一步"),
+              ),
+              FlatButton(
+                child: Text("下一步"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          _createBackground(),
+          _createTabs(),
+        ],
       ),
     );
   }
