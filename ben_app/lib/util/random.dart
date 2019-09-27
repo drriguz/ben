f700ffd8913e332bfbe1cc66ca8148b6ee4dd1b9
Riguz Lee <solee.linux@gmail.com>
2019-09-27 11:01:20 +0800

    using secure random instead of uuid

--- ben_app/lib/util/random.dart
+++ ben_app/lib/util/random.dart
@@ -1,16 +1,21 @@
-import 'package:uuid/uuid.dart';
+import 'dart:math';
+import 'dart:typed_data';
+
 import 'package:convert/convert.dart';
 
 class RandomStringUtil {
-  static final _uuid = new Uuid();
+  static final _random = Random.secure();
 
   static String generateUUID() {
     return hex.encode(generateUUIDasBytes());
   }
 
   static List<int> generateUUIDasBytes() {
-    final uuid = new List<int>(16);
-    _uuid.v4buffer(uuid);
-    return uuid;
+    return randomBytes(32);
+  }
+
+  static Uint8List randomBytes(int length) {
+    return Uint8List.fromList(
+        List.generate(length, (i) => _random.nextInt(0xff)));
   }
 }
