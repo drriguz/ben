9ac69e04a99abcd23757661f5acc0fb7720539ec
Riguz Lee <solee.linux@gmail.com>
2019-10-10 13:50:10 +0800

    refactor

--- ben_app/lib/ui/widget/bank_card_item.dart
+++ ben_app/lib/ui/widget/bank_card_item.dart
@@ -1,68 +1,25 @@
 import 'package:ben_app/ui/model/bank_card.dart';
+import 'package:ben_app/util/strings.dart';
 import 'package:flutter/material.dart';
 
 import 'list_item.dart';
 
-class BankCardItem extends AbstractListItem<BankCard> {
+class BankCardItem extends GeneralIconItem<BankCard> {
   const BankCardItem({Key key, BankCard model})
-      : super(key: key, model: model, hasDescription: true);
+      : super(key: key, model: model);
 
   @override
-  Widget buildDescription() {
-    return Text(
-      _getDisplayCardNumber(),
-      style: TextStyle(
-        fontSize: 20,
-        color: Colors.black,
-      ),
-    );
+  String getDescription() {
+    return Strings.masking(model.number);
   }
 
   @override
-  Widget buildIcon() {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
-      child: Image(
-        image: AssetImage('assets/banks/cmb.png'),
-        width: 32,
-        height: 32,
-      ),
-    );
+  ImageProvider getItemIcon() {
+    return AssetImage('assets/banks/cmb.png');
   }
 
   @override
-  Widget buildTitle() {
-    return Container(
-      padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-      decoration: BoxDecoration(
-        border: Border(
-            left: BorderSide(
-          color: Colors.orange,
-        )),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            model.title,
-            style: TextStyle(
-              fontSize: 14,
-              color: Colors.black,
-            ),
-          ),
-          Text(
-            _getCardTypeName(),
-            style: TextStyle(
-              fontSize: 14,
-              color: Colors.grey,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  String _getCardTypeName() {
+  String getSubtitle() {
     switch (model.type) {
       case CardType.CREDIT:
         return "信用卡";
@@ -72,10 +29,8 @@ class BankCardItem extends AbstractListItem<BankCard> {
     throw new Exception("Unexpected card type");
   }
 
-  String _getDisplayCardNumber() {
-    String number = "****";
-    if (model.number.length > 4)
-      number += " " + model.number.substring(model.number.length - 4);
-    return number;
+  @override
+  String getTitle() {
+    return model.title;
   }
 }
