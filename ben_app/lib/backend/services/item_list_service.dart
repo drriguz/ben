0c85d2c6bd2495239cf41640f029799967dec4ad
Riguz Lee <solee.linux@gmail.com>
2019-10-28 08:55:26 +0800

    deleted unused method

--- ben_app/lib/backend/services/item_list_service.dart
+++ ben_app/lib/backend/services/item_list_service.dart
@@ -4,7 +4,6 @@ import 'package:ben_app/format/data_format.dart';
 import 'package:ben_app/format/serialize.dart';
 import 'package:ben_app/format/sqlite/Item_entity.dart';
 import 'package:ben_app/format/storage.dart';
-import 'package:ben_app/ui/model/bank_card_model.dart';
 import 'package:uuid/uuid.dart';
 
 class ItemListService {
@@ -12,13 +11,6 @@ class ItemListService {
   final uuid = new Uuid();
 
   ItemListService();
-
-  Future<List<BankCardModel>> fetch() async {
-    print('fetching...');
-    final items = await itemRepository.getItems();
-    print('fetched: ${items.length}');
-    return items.map((item) => BankCardModel(title: item.id)).toList();
-  }
 
   Future<List<Item>> fetchByType(int type) async {
     await Future.delayed(Duration(seconds: 1));
